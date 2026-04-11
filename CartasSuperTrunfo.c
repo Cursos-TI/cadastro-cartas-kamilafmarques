c2d2c221263798e64780df0294b05c65f4a466f5
kamilafmarques <kamilakgdebe@hotmail.com>
2026-04-11 02:28:58 +0000

    continuo nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,21 +14,27 @@ int main() {
     char Cidade1 [20];
     int População1;
     float Área1;
-    double PIB1;
+    float PIB1;
     int Pontos1;
+    float Densidade1;
+    float PIBpercapita1;
 
 
-  // Área para definição das variáveis para armazenar as propriedades das cidades
     char Estado2;
     char Carta2 [4];
     char Cidade2 [20];
     int População2;
     float Área2;
-    double PIB2;
+    float PIB2;
     int Pontos2;
-    
+    float Densidade2;
+    float PIBpercapita2;
+
+ 
+  
+  
+  
      
-    
 
   // Área para entrada de dados
   printf ("Vamos começar o jogo Super Trunfo!\n");
@@ -38,18 +44,34 @@ int main() {
   printf ("Digite o Estado - Letra de A a H reprentando um dos oito estados: \n");
   scanf ("%c", &Estado1);
   printf ("Digite o Código da Carta - Letra do estado seguida de um número de 01 a 04: \n");
-  scanf ("%s", &Carta2);
+  scanf ("%s", Carta1);
   printf ("Digite o nome da Cidade:\n");
-  scanf ("%s", &Cidade1);
+  scanf ("%s", Cidade1);
   printf ("Digite o número de habitantes da cidade: \n");
   scanf ("%d", &População1);
   printf ("Digite a área da cidade em quilômetros quadrado:\n");
-  scanf ("%f", &Área1);
+  scanf ("%.2f", &Área1);
   printf ("Digite o PIB da cidade: \n");
-  scanf ("%2.f", &PIB1);
+  scanf ("%.2f", &PIB1);
   printf ("Digite a quantidade de pontos turísticos na cidade: \n");
   scanf ("%d", &Pontos1);
-   
+
+  printf ("Carta 2\n");
+  printf ("Digite o Estado - Letra de A a H reprentando um dos oito estados: \n");
+  scanf ("%c", &Estado2);
+  printf ("Digite o Código da Carta - Letra do estado seguida de um número de 01 a 04: \n");
+  scanf ("%s", Carta2);
+  printf ("Digite o nome da Cidade:\n");
+  scanf ("%s", Cidade2);
+  printf ("Digite o número de habitantes da cidade: \n");
+  scanf ("%d", &População2);
+  printf ("Digite a área da cidade em quilômetros quadrado:\n");
+  scanf ("%.2f", &Área2);
+  printf ("Digite o PIB da cidade: \n");
+  scanf ("%.2f", &PIB2);
+  printf ("Digite a quantidade de pontos turísticos na cidade: \n");
+  scanf ("%d", &Pontos2);
+  
 
   // Área para exibição dos dados da cidade
   
@@ -64,31 +86,9 @@ int main() {
   printf ("Número de Pontos Turísticos: %d\n", Pontos1);
   float Densidade1 = População1 / Área1;
   printf ("Densidade Populacional: %.2f\n", Densidade1);
-  double PIBpercapita1 = (double)PIB1 / População1;
+  float PIBpercapita1 = (float) PIB1 / População1;
   printf ("PIB per Capita: %.2f\n", PIBpercapita1);
-
-// Área para entrada de dados
-
-
-  printf ("Carta 2\n");
-
-  printf ("Digite o Estado - Letra de A a H representando um dos oito estados: \n");
-  scanf ("%c", &Estado2);
-  printf ("Digite o Código da Carta - Letra do estado seguida de um número de 01 a 04: \n");
-  scanf ("%s", &Carta2);
-  printf ("Digite o nome da Cidade:\n");
-  scanf ("%s", &Cidade2);
-  printf ("Digite o número de habitantes da cidade: \n");
-  scanf ("%f", &População2);
-  printf ("Digite a área da cidade em quilômetros quadrado:\n");
-  scanf ("%f", &Área2);
-  printf ("Digite o PIB da cidade: \n");
-  scanf ("%f", &PIB2);
-  printf ("Digite a quantidade de pontos turísticos na cidade: \n");
-  scanf ("%d", &Pontos2);
-
-    // Área para exibição dos dados da cidade
-
+  
   printf ("Carta 2\n");
 
   printf ("Estado: %c\n", Estado2);
@@ -100,7 +100,7 @@ int main() {
   printf ("Número de Pontos Turísticos: %d\n", Pontos2);
   float Densidade2 = População2 / Área2;
   printf ("Densidade Populacional: %.2f\n", Densidade2);
-  double PIBpercapita2 = (double) PIB2 / População2;
+  float PIBpercapita2 = (float) PIB2 / População2;
   printf ("PIB per Capita: %.2f\n", PIBpercapita2);
 
 
